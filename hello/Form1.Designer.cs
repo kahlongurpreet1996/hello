1c491b8eed3e3fe76edad1ac135a58c4b330436c
Gurpreet Kaur <GK008@atcltd.atc.org.nz>
2019-08-13 13:16:25 +1200

    test

---
+++ hello/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace hello
+{
+    partial class hello
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // hello
+            // 
+            this.ClientSize = new System.Drawing.Size(1325, 569);
+            this.Name = "hello";
+            this.Load += new System.EventHandler(this.Hello_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.ListBox lbxOutput;
+    }
+}
+
---
+++ hello/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hello
+{
+    public partial class hello : Form
+    {
+        public hello()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            //lblOutput.Text = "Hello Gurpreet"; //THis is to get the output text to the lable whose name is lblOutput
+            lbxOutput.Items.Add("Hello Gurpreet"); //The lisbox item Hello Gurpreet is added
+            string Name = "'bikash pokhrel":
+        }
+    }
+    }
+}
